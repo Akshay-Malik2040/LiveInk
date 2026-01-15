10704c193ddddc4b4c69ba2dd519939af4e2b656
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2026-01-15 19:09:36 +0530

    Socket Manager init

---
+++ src/socket/socketManager.js
@@ -0,0 +1,20 @@
+const {Server}=require('socket.io');
+
+const initSocket=(server)=>{
+    const io=new Server(server,{
+        cors:{
+            origin:"*"
+        }
+    });
+
+    io.on('connection',(socket)=>{
+        console.log('User connected: ',socket.id);
+
+        socket.on('disconnect',()=>{
+            console.log('User disconnected: ',socket.id);
+        })
+    });
+    return io;
+}
+
+module.exports=initSocket;
