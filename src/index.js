2a7bad1171dfac3e65bc0aef673937ea856d840a
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2026-01-15 19:13:52 +0530

    Initialize server on 3000 port

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const http=require('http');
+const initSocket=require('./socket/socketManager');
+require('./redis/redisClient');
+
+const app=express();
+const server=http.createServer(app);
+
+initSocket(server);
+
+server.listen(3000,()=>{
+    console.log('Server running on port 3000');
+});
